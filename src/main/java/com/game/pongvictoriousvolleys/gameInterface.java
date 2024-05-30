2f3b9d453b8f022bd5a0666c2ebdda59104f69d0
AanojanB <aanojan.bharathkumar@student.tdsb.on.ca>
2024-05-30 08:31:01 -0400

    Initial commit

---
+++ src/main/java/com/game/pongvictoriousvolleys/gameInterface.java
@@ -0,0 +1,39 @@
+package com.game.pongvictoriousvolleys;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class gameInterface {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void backButton(ActionEvent event) throws IOException {
+
+        root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public void playButton(ActionEvent event) throws IOException {
+
+        root = FXMLLoader.load(getClass().getResource("gameFrame.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+}
+
